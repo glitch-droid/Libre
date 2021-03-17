35c1857ebe42cff6b1c68e91d2d58861836aaac7
Mriganka Bharali <mrigankabharali30@gmail.com>
2021-03-18 00:14:42 +0530

    Login_Layout_1

--- app/src/main/java/com/example/libre/MainActivity.java
+++ app/src/main/java/com/example/libre/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
