21b40bac6b1b2d75586026c3ba7820a2dddb3dee
Mriganka Bharali <mrigankabharali30@gmail.com>
2021-03-18 12:39:42 +0530

    Register Layout - 1

--- app/src/main/java/com/example/libre/MainActivity.java
+++ app/src/main/java/com/example/libre/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.register_activity);
         Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
