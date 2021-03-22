00eb6e2ccf7c62736fc22f3a85fcfd3c292b5005
Snehashish2403 <snehashishkarmakar2403@gmail.com>
2021-03-22 12:54:10 +0530

    All products fetched into recyclerview

--- app/src/main/java/com/example/libre/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/libre/Fragments/HomeFragment.java
@@ -15,14 +15,29 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.libre.Adapters.HomeAdapter;
+import com.example.libre.MainActivity;
 import com.example.libre.Models.BookModel;
+import com.example.libre.MyApplication;
 import com.example.libre.R;
+import com.example.libre.Retrofit_Modules.API_Caller;
+import com.example.libre.Retrofit_Modules.Models.CurrentUser;
+import com.example.libre.Retrofit_Modules.Models.Products;
+import com.example.libre.Retrofit_Modules.Models.UserDetails;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class HomeFragment extends Fragment {
+
+    private Retrofit retrofit;
 
     private boolean clicked = false;
     @Nullable
@@ -35,25 +50,42 @@ public class HomeFragment extends Fragment {
         HomeAdapter adapter = new HomeAdapter();
         List<BookModel> bookModelList = new ArrayList<>();
 
-        String rr = String.valueOf(R.string.loren_ipsum);
+        API_Caller caller=retrofit.create(API_Caller.class);
+        Call<UserDetails> call=caller.getUserDetailsAfterLogin("products/?xerox=book");
+        call.enqueue(new Callback<UserDetails>() {
+            @Override
+            public void onResponse(Call<UserDetails> call, Response<UserDetails> response) {
+                if(response.isSuccessful()){
+                    List<Products> books=response.body().getProducts();
+                    CurrentUser user=response.body().getCurrentUser();
+                    System.out.println("CURRENT USER: "+user.getUsername());
+                    for(Products prod:books){
+                        if(prod.getImage().size()!=0){
+                            bookModelList.add(new BookModel(prod.getTitle(),prod.getBookauthor(),prod.getDescription(),String.valueOf(prod.getAmount()),prod.getImage().get(0),prod.getId()));
+                        }else{
+                            bookModelList.add(new BookModel(prod.getTitle(),prod.getBookauthor(),prod.getDescription(),String.valueOf(prod.getAmount()),"",prod.getId()));
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
 
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
-        bookModelList.add(new BookModel("Modern Control Engineering","By  Katsuhiko Ogata",rr,"₹559"));
+            @Override
+            public void onFailure(Call<UserDetails> call, Throwable t) {
+                System.out.println("ERROR: "+t);
+            }
+        });
         adapter.setBooksList(bookModelList);
-
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         recyclerView.hasFixedSize();
+        adapter.notifyDataSetChanged();
 
         return view;
     }
 
 
-    public HomeFragment() {
+    public HomeFragment(Retrofit retrofit) {
+        this.retrofit=retrofit;
     }
 }
--- app/src/main/java/com/example/libre/Models/BookModel.java
+++ app/src/main/java/com/example/libre/Models/BookModel.java
@@ -6,14 +6,18 @@ public class BookModel {
     String author;
     String description;
     String price;
+    String url;
+    String id;
     public BookModel() {
     }
 
-    public BookModel(String book, String author, String description, String price) {
+    public BookModel(String book, String author, String description, String price,String url,String id) {
         this.book = book;
         this.author = author;
         this.description = description;
         this.price = price;
+        this.id=id;
+        this.url=url;
     }
 
     public String getBook() {
@@ -46,5 +50,21 @@ public class BookModel {
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
