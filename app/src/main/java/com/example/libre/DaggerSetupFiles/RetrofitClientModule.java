25b9fe334f0e024ca47d965ac1a3712ada8eefe3
Snehashish2403 <snehashishkarmakar2403@gmail.com>
2021-03-28 02:20:08 +0530

    Fixed some bugs

--- app/src/main/java/com/example/libre/DaggerSetupFiles/RetrofitClientModule.java
+++ app/src/main/java/com/example/libre/DaggerSetupFiles/RetrofitClientModule.java
@@ -1,7 +1,10 @@
 package com.example.libre.DaggerSetupFiles;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
+import com.airbnb.lottie.utils.Utils;
 import com.example.libre.Constants.Constants;
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
@@ -9,14 +12,23 @@ import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersisto
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.CacheControl;
 import okhttp3.CookieJar;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -24,15 +36,18 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 @Module
 public class RetrofitClientModule {
+
+
     Context context;
-    public RetrofitClientModule(Context context){
-        this.context=context;
+
+    public RetrofitClientModule(Context context) {
+        this.context = context;
     }
 
     @Provides
     @Singleton
-    Retrofit RetrofitClientProvider(){
-        HttpLoggingInterceptor loggingInterceptor=new HttpLoggingInterceptor();
+    Retrofit RetrofitClientProvider() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
         Gson gson = new GsonBuilder()
@@ -40,19 +55,22 @@ public class RetrofitClientModule {
                 .create();
 
 
+        File cacheFile = new File(context.getCacheDir(), "responses");
+        long cacheSize = 10 * 1024 * 1024;
+        Cache cache = new Cache(cacheFile, cacheSize);
 
-        CookieJar cookieJar=new PersistentCookieJar(new SetCookieCache(),new SharedPrefsCookiePersistor(context));
+        CookieJar cookieJar = new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(context));
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .connectTimeout(60*5, TimeUnit.MINUTES)
-                .readTimeout(30*5, TimeUnit.SECONDS)
+                .cache(cache)
+                .connectTimeout(60 * 5, TimeUnit.MINUTES)
+                .readTimeout(30 * 5, TimeUnit.SECONDS)
                 .retryOnConnectionFailure(true)
                 .cookieJar(cookieJar)
-                .writeTimeout(15*5, TimeUnit.SECONDS)
+                .writeTimeout(15 * 5, TimeUnit.SECONDS)
                 .addInterceptor(loggingInterceptor)
                 .build();
-
-        Retrofit retrofit=new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
@@ -60,4 +78,10 @@ public class RetrofitClientModule {
                 .build();
         return retrofit;
     }
+
+
+
+
+
 }
+
